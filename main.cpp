cf39d4955ea81b699fbd127830933b0c5f1293ab
Harsh Mathur <harshmathur.1990@gmail.com>
2019-12-26 16:46:15 +0530

    working code but returning junk values, needs to be tested against a real source

--- main.cpp
+++ main.cpp
@@ -24,12 +24,12 @@ int main() {
         std::cout<<"The value of sample voltages to be get are: "<< numOfVoltageSamples<<std::endl;
         float64 *readArray = NULL;
         readArray = (float64*)malloc(numOfVoltageSamples * sizeof(float64));
-        int32 *samplesReadperChannel = NULL;
-        int status = getVoltage(readArray, samplesReadperChannel, numOfVoltageSamples);
+        int32 samplesReadperChannel = 0;
+        int status = getVoltage(readArray, &samplesReadperChannel, numOfVoltageSamples);
         std::cout<<"The status from reading input samples is "<<status<<std::endl;
-        std::cout<<"The number of samples read are " << *samplesReadperChannel << std::endl;
-        if (status) {
-            for (int i=0; i < *samplesReadperChannel; i++) {
+        std::cout<<"The number of samples read are " << samplesReadperChannel << std::endl;
+        if (status == 0) {
+            for (int i=0; i < samplesReadperChannel; i++) {
                 std::cout << "Voltage[" << i << "] = " << readArray[i] << std::endl;
             }
         }
